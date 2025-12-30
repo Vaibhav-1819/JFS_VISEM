faa7961f7e2419174b13cb8627c4666ec3fcf71d
Vaibhav <bharathulavaibhav@gmail.com>
2025-12-30 11:39:08 +0530

    Update hibernate-student

--- maven_projects/hibernate-student/src/main/java/com/skillnext2/App.java
+++ maven_projects/hibernate-student/src/main/java/com/skillnext2/App.java
@@ -16,7 +16,7 @@ public class App {
         while (running) {
 
             System.out.println("\n===== STUDENT MANAGEMENT SYSTEM =====");
-            System.out.println("1. Add Student");
+            System.out.println("1. Add Student Details");
             System.out.println("2. View All Students");
             System.out.println("3. Update Student");
             System.out.println("4. Delete Student");
